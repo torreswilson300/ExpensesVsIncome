e2f9c1643059bbe79d76a28dae86ff8f3cbaa1b2
torreswilson300 <tcwilson@aggies.ncat.edu>
2025-05-26 13:33:43 -0400

    nodemon server script

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
 import express from 'express';
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend server!');
+}
+);
+
+app.listen(5001, () => {
+    console.log('Server is running on port 5001');
+});
