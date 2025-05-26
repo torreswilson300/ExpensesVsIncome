5bd6c57a71483281cbcb665ae9aa78a6a2dbf1ba
torreswilson300 <tcwilson@aggies.ncat.edu>
2025-05-26 17:21:32 -0400

    add express middleware for JSON parse, and ability to post transactions to the db

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const app = express();
 // Set the port to the value from the environment variable (.env) or default to 5001
 const PORT = process.env.PORT || 5001;
 
+app.use(express.json()); // Middleware to parse JSON request bodies
+
 async function connectToDatabase() {
     try{
         await sql`CREATE TABLE IF NOT EXISTS transactions (
@@ -27,13 +29,30 @@ async function connectToDatabase() {
         process.exit(1); // Exit the process with an error code 
     }
 }
-
 //DECIMAL(10,2) allows for 10 digits in total, with 2 digits after the decimal point, 999999999.99 (8 digits before the decimal point and 2 after)
 
-app.get('/', (req, res) => {
-    res.send('Hello from the backend server!');
-}
-);
+app.post('/api/transactions', async (req, res) => {
+    //title, amount, category, user_id
+    try {
+        // Destructure the required fields from the request body
+        const{title, amount, category, user_id} = req.body;
+        // Validate that all required fields are present
+        if (!title || amount === undefined || !category || !user_id) {
+            return res.status(400).json({ error: 'All fields are required' });
+        }
+        // Insert the new transaction into the database
+        const transaction =
+        await sql`INSERT INTO transactions (user_id, title, amount, category)
+        VALUES (${user_id}, ${title}, ${amount}, ${category})
+        RETURNING *
+        `
+        res.status(201).json(transaction[0]); // Respond with the created transaction
+    } catch (error) {
+        console.log ("Error in creating transaction:", error);
+        res.status(500).json({ error: 'Internal server error' });
+        
+    }
+});
 
 
 //initialize the database connection then start the server
